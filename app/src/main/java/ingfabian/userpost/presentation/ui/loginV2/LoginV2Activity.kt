7376395bf8a14a6834cb66a514bde45ddabc3183
Fabian Mondragon <fabianmondragon@Fabians-MacBook-Pro.local>
2021-02-21 18:56:24 -0500

    feature: design login App

---
+++ app/src/main/java/ingfabian/userpost/presentation/ui/loginV2/LoginV2Activity.kt
@@ -0,0 +1,12 @@
+package ingfabian.userpost.presentation.ui.loginV2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ingfabian.userpost.R
+
+class LoginV2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_v2)
+    }
+}
